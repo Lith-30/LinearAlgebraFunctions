8d24211694d850153e9f6c9ad81ed1cf9e387722
Lithira <z5477388@ad.unsw.edu.au>
2024-09-04 12:23:23 +1000

    Fixed errors in the matrix.c file, completed the determinant function and created the displayMatrix function to print out a matrix

--- matrix.c
+++ matrix.c
@@ -3,33 +3,89 @@
 #include <math.h>
 #include <stdlib.h>
 #include <error.h>
+#include <string.h>
+
+#define MAX_DIGITS 10
 
 typedef struct {
 	int rows;
 	int cols;
-	int **matrix;
+	float **matrix;
 } Matrix;
 
 typedef Matrix* M;
 
-// create a new n (rows) by m (columns) matrix
-M newMatrix(int n, int m) {
-	M m = malloc(sizeof(Matrix));
-	m->rows = n;
-	m->cols = m;
-	m->matrix = malloc(sizeof(int *) * n);
-	for (int i = 0; i < n; i++) {
-		m->matrix[i] = malloc(sizeof(int) * m);
+M newMatrix(int n, int m);
+M transposeMatrix(M m);
+int getElement(M m, int i, int j);
+int *getRow(M m, int row);
+int *getCol(M m, int col);
+M multiplyMatrix(M m1, M m2);
+M copyMatrix(M m);
+void insertValue(M m, int i, int j, float val);
+void rewriteMatrix(M m, float *vals, int num_vals);
+void overwriteMatrix(M source, M sink);
+void rowEchelonForm(M m);
+void rowReduce(M m, int row1, int row2, float factor);
+int determinant(M m);
+void freeMatrix(M m);
+void displayMatrix(M m);
+
+int main(void) {
+	return 0;
+}
+
+void displayMatrix(M m) {
+	int longestNumDigits = 0;
+	char buffer[MAX_DIGITS + 1];
+	for (int i = 0; i < m->rows; i++) {
+		for (int j = 0; j < m->cols; j++) {
+			gcvt(m->matrix[i][j], MAX_DIGITS + 1, buffer);
+			int len = strlen(buffer);
+			longestNumDigits = longestNumDigits * (longestNumDigits > len) + len * (len <= longestNumDigits);
+		}
 	}
 
-	return m;
+	// so to leave at least one space between all elements we must have longestNumDigits + 1 space for each number
+	for (int i = 0; i < m->rows; i++) {
+		printf("| ");
+		for (int j = 0; j < m->cols; j++) {
+			gcvt(m->matrix[i][j], MAX_DIGITS + 1, buffer);
+			int len = strlen(buffer);
+			//display number
+			printf("%s", buffer);
+			// insert appropriate spacings
+			for (int spaces = 0; spaces < longestNumDigits - len + 1; spaces++) {
+				printf(" ");
+			}
+		}
+		printf("|\n");
+	}
+	
+	return;
+}
+
+// create a new n (rows) by m (columns) 0 matrix
+M newMatrix(int n, int m) {
+	M new = malloc(sizeof(Matrix));
+	new->rows = n;
+	new->cols = m;
+	new->matrix = malloc(sizeof(float *) * n);
+	for (int i = 0; i < n; i++) {
+		new->matrix[i] = malloc(sizeof(float) * m);
+		for (int j = 0; j < new->cols; j++) {
+			new->matrix[i][j] = 0;
+		}
+	}
+
+	return new;
 }
 
 // updates the matrix to be its own transpose
 M transposeMatrix(M m) {
 	// matrix is a square matrix transpose is ez
 	if (m->rows == m->cols) {
-		int half = m->row / 2;
+		int half = m->rows / 2;
 		for (int j = 0; j < m->cols; j++) {
 			for (int i = 0; i <= j; i++) {
 				int temp = m->matrix[i][j];
@@ -43,7 +99,7 @@ M transposeMatrix(M m) {
 		M newM = newMatrix(m->cols, m->rows);
 		for (int i = 0; i < m->cols; i++) {
 			for (int j = 0; j < m->rows; j++) {
-				newM->matrix[i][j] = m->[j][i];
+				newM->matrix[i][j] = m->matrix[j][i];
 			}
 		}
 		freeMatrix(m);
@@ -60,7 +116,7 @@ int getElement(M m, int i, int j) {
 		exit(EXIT_FAILURE);
 	}
 
-	return m->[i - 1][j - 1];
+	return m->matrix[i - 1][j - 1];
 }
 
 // returns the requested row in a array
@@ -70,27 +126,27 @@ int *getRow(M m, int row) {
 		exit(EXIT_FAILURE);
 	}
 
-	int *row = malloc(sizeof(int) * m->rows);
+	int *rows = malloc(sizeof(int) * m->rows);
 	for (int j = 0; j < m->cols; j++) {
-		row[j] = m->matrix[row][j];
+		rows[j] = m->matrix[row][j];
 	}
 
-	return row;
+	return rows;
 }
 
 // returns the requested column in a array
-int *getRow(M m, int col) {
-	if (col < 1 || row > m->cols) {
+int *getCol(M m, int col) {
+	if (col < 1 || col > m->cols) {
 		fprintf(stderr, "Requested matrix col out of bounds");
 		exit(EXIT_FAILURE);
 	}
 
-	int *row = malloc(sizeof(int) * m->cols);
+	int *cols = malloc(sizeof(int) * m->cols);
 	for (int i = 0; i < m->rows; i++) {
-		row[i] = m->matrix[i][col];
+		cols[i] = m->matrix[i][col];
 	}
 
-	return row;
+	return cols;
 }
 
 M multiplyMatrix(M m1, M m2) {
@@ -100,33 +156,119 @@ M multiplyMatrix(M m1, M m2) {
 	}
 	M result = newMatrix(m1->rows, m2->cols);
 	
-	for (int i = 0; i < result->row; i++) {
-		for (int j = 0; j < result->col; j++) {
+	for (int i = 0; i < result->rows; i++) {
+		for (int j = 0; j < result->cols; j++) {
 			int prodsum = 0;
 			for (int k = 0; k < m1->cols; k++) {
-				prodsum += m1[i][k] * m2[k][j];
+				prodsum += m1->matrix[i][k] * m2->matrix[k][j];
 			}
-			result[i][j] = prodsum;
+			result->matrix[i][j] = prodsum;
 		}
 	}
 
 	return result;
 }
 
-// in progress
-//int determinant(M m) {
-//	if (m->rows != m->cols) {
-//		fprintf(stderr, "Determinant is not defined for non-square matrices");
-//		exit(EXIT_FAILURE);
-//	}
-//}
+// return a copy of a matrix
+M copyMatrix(M m) {
+	M new = newMatrix(m->rows, m->cols);
+	for (int i = 0; i < m->rows; i++) {
+		for (int j = 0; j < m->cols; j++) {
+			new->matrix[i][j] = m->matrix[i][j];
+		}
+	}
+
+	return new;
+}
+
+// update or insert a new value into a given position in a matrix
+void insertValue(M m, int i, int j, float val) {
+	m->matrix[i - 1][j - 1] = val;
+	return;
+}
+
+// update the first nm_vals values of a matrix
+void rewriteMatrix(M m, float *vals, int num_vals) {
+	for (int i = 0; i < num_vals; i++) {
+		m->matrix[i / m->cols][i % m->cols] = vals[i];
+	}
+
+	return;
+}
+
+// copies all of the entries from source to sink
+void overwriteMatrix(M source, M sink) {
+	if (source->rows != sink->rows || source->cols != sink->cols) {
+		fprintf(stderr, "matrix row and column sizes do not match");
+		exit(EXIT_FAILURE);
+	}
+
+	for (int i = 0; i < source->rows; i++) {
+		for (int j = 0; j < source->cols; j++) {
+			sink->matrix[i][j] = source->matrix[i][j];
+		}
+	}
+}
+
+void rowEchelonForm(M m) {
+	M copy = copyMatrix(m);
+
+	for (int j = 0; j < copy->cols; j++) {
+		for (int i = j + 1; i < copy->rows; i++) {
+			if (copy->matrix[i][j] != 0 && copy->matrix[j][j] != 0) {
+				rowReduce(copy, j, i, -1 * m->matrix[i][j] / m->matrix[j][j]);	
+			}
+		}
+	}
+
+	return;
+}
+
+void rowReduce(M m, int row1, int row2, float factor) {
+	// generate a matrix to perform the row reduction
+	M red = newMatrix(m->rows, m->rows);
+	red->matrix[row2][row1] = factor;
+	// set new matrix to be en identity matrix
+	for (int i = 0;  i < m->rows; i++) {
+		red->matrix[i][i] = 1;
+	}
+
+	M result = multiplyMatrix(red, m);
+	
+	overwriteMatrix(result, m);
+
+	freeMatrix(result);
+
+	return;
+}
+
+// find the determinant of a matrix
+int determinant(M m) {
+	if (m->rows != m->cols) {
+		fprintf(stderr, "Determinant is not defined for non-square matrices.");
+		exit(EXIT_FAILURE);
+	}
+	// create a copy to get row reduced
+	M copy = copyMatrix(m);
+
+	rowEchelonForm(copy);
+	
+	int det = m->matrix[0][0];
+	
+	// multiply the diagonal
+	for (int i = 1; i < m->rows; i++) {
+		det *= m->matrix[i][i];
+	}
+
+	return det;
+}
 
 //free matrix to conserve memory
 void freeMatrix(M m) {
 	for (int i = 0; i < m->rows; i++) {
 		free(m->matrix[i]);
 	}
-	free(matrix);
+	free(m->matrix);
 	free(m);
 
 	return;
